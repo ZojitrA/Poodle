485876d51cca628e7db3b66ed5e6ae57ba5d6032
Soji <ahsojitra@gmail.com>
2019-02-21 14:08:13 -0500

    fingers crossed again

--- config.php
+++ config.php
@@ -8,10 +8,10 @@ $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server   = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
-
+$cleardb_db       = substr($cleardb_url["path"],1);
 //"root" in place of username, "" in place of password
 
-$con = new PDO("mysql:dbname=poodle;host=".$cleardb_server, $cleardb_username, $cleardb_password);
+$con = new PDO("mysql:dbname=".$cleardb_db.";host=".$cleardb_server, $cleardb_username, $cleardb_password);
 $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
 }
